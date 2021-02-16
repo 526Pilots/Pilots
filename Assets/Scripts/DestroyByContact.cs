680b52b4ad30197c2e0b010ba23de9acaab7261f
Linchenn <40653845+Linchenn@users.noreply.github.com>
2021-02-15 23:07:47 -0800

    enemy

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+	public float tumble;
+    //当其他碰撞器进入当前GameObject的触发器时，销毁该碰撞器对应的游戏对象，同时销毁该GameObject
+    void OnTriggerEnter(Collider other)
+    {
+		// if(other.tag == "Boundary") 
+		// {
+		// 	return;
+		// }
+		
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float speed;
+	public float tumble;
+	private Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = -1 * Vector3.forward * speed;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
