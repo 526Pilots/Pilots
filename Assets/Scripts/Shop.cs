a64bf70a227cd9aa92c6d79fa233fb1568617be4
Linchenn <40653845+Linchenn@users.noreply.github.com>
2021-04-20 16:14:15 -0700

    add shop

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Shop : MonoBehaviour
+{
+    void Start()
+    {   
+        
+    }
+    
+    public void BuyHealth()
+    {  
+        
+    }
+
+    public void BuyMoveSpeed()
+    {  
+        
+    }
+
+    public void BuyAttackSpeed()
+    {  
+        
+    }
+
+    public void backToMain() {
+        SceneManager.LoadScene(0);
+    }
+}
+
